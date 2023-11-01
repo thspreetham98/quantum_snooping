45ab6e2bf03b1993e9a07380e3560d31d45e196c
Preetham Tikkireddi <thspreetham@gmail.com>
2023-11-01 11:32:02 -0500

    refactor and 1_3 exp1

--- common/analysis_helper.py
+++ common/analysis_helper.py
@@ -4,6 +4,8 @@ from qiskit.result import Result, marginal_counts
 import os
 import tempfile
 import shutil
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_results_from_zip(folder_path, job_id) -> Result:
@@ -21,3 +23,64 @@ def get_results_from_zip(folder_path, job_id) -> Result:
 def get_1_mcounts_per_qubit(atk_qbt_index, counts):
     m_counts = [marginal_counts(count, indices=[atk_qbt_index]).get('1', 0) for count in counts]
     return m_counts
+
+def exp_1_diff_heatmap(attack_qubit_indices, delays, diff_percent_xs, diff_percent_zs, cnots):
+    # # Sample data - replace this with your actual data
+    qubits = ['Qubit {}'.format(i) for i in attack_qubit_indices]
+    experiments = ['Delay {} dt'.format(i) for i in delays]
+
+    # Generate random data for each experiment
+    data1 = np.array(diff_percent_xs)
+    data2 = np.array(diff_percent_zs)
+
+    # Calculate the common data range for both heatmaps
+    data_min = min(data1.min(), data2.min())
+    data_max = max(data1.max(), data2.max())
+
+    # Create a figure with 1 row and 2 columns
+    fig, ax = plt.subplots(1, 2, figsize=(16, 6))  # 1 row, 2 columns
+    fig.suptitle('IBM Lagos CNOT {}: Different snooping durations and basis'.format(cnots))
+
+    # Determine a common scale for both heatmaps
+    common_vmin = min(data_min, data2.min())
+    common_vmax = max(data_max, data2.max())
+
+    # Plot the first heatmap on the left subplot (ax[0])
+    heatmap1 = ax[0].imshow(data1, cmap='viridis', aspect='auto', vmin=common_vmin, vmax=common_vmax)
+    ax[0].set_title('Phase Flips')
+    ax[0].set_xticks(np.arange(len(experiments)))
+    ax[0].set_yticks(np.arange(len(qubits)))
+    ax[0].set_xticklabels(experiments, rotation=45, ha='right')
+    ax[0].set_yticklabels(qubits)
+
+    # Display percentage values inside the boxes
+    for i in range(len(qubits)):
+        for j in range(len(experiments)):
+            ax[0].text(j, i, f'{data1[i, j]:.2f}%', ha='center', va='center', color='w')
+
+    # Plot the second heatmap on the right subplot (ax[1])
+    heatmap2 = ax[1].imshow(data2, cmap='viridis', aspect='auto', vmin=common_vmin, vmax=common_vmax)
+    ax[1].set_title('Amplitude Flips')
+    ax[1].set_xticks(np.arange(len(experiments)))
+    ax[1].set_yticks(np.arange(len(qubits)))
+    ax[1].set_xticklabels(experiments, rotation=45, ha='right')
+    ax[1].set_yticklabels(qubits)
+
+    # Display percentage values inside the boxes
+    for i in range(len(qubits)):
+        for j in range(len(experiments)):
+            ax[1].text(j, i, f'{data2[i, j]:.2f}%', ha='center', va='center', color='w')
+
+    # Add colorbars to both heatmaps
+    cbar1 = fig.colorbar(heatmap1, ax=ax[0])
+    cbar2 = fig.colorbar(heatmap2, ax=ax[1])
+
+    # Set labels for the colorbars
+    cbar1.set_label('Percentage Difference in 1 counts')
+    cbar2.set_label('Percentage Difference in 1 counts')
+
+    # Adjust the layout to prevent clipping of the labels
+    plt.tight_layout()
+
+    return fig, ax
+
--- common/attacker.py
+++ common/attacker.py
@@ -1,40 +1,57 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.circuit import Parameter
 
-def single_snooping_window(num_of_qubits: int) -> QuantumCircuit:
-    assert num_of_qubits > 0
-    qregs = QuantumRegister(num_of_qubits)
-    cregs = ClassicalRegister(num_of_qubits)
-    delay_dt = Parameter('snooping duration')
-    qc = QuantumCircuit(qregs, cregs)
-    qc.h(qregs)
-    qc.delay(delay_dt, qregs)
-    qc.h(qregs)
+def measure_and_conditional_reset(qc: QuantumCircuit, qregs: QuantumRegister, cregs: ClassicalRegister):
     qc.measure(qregs, cregs)
-    return qc
-
-def conditional_reset(num_of_qubits: int):
-    qregs = QuantumRegister(num_of_qubits)
-    cregs = ClassicalRegister(num_of_qubits)
-    qc = QuantumCircuit(qregs, cregs, name="Conditional Reset")
-    for i in range(num_of_qubits):
-        with qc.if_test((cregs[i], 1)):
+    for i, creg in enumerate(cregs):
+        with qc.if_test((creg, 1)):
             qc.x(qregs[i])
-    return qc
+    return qc    
 
-def create_snooping_circuit(num_of_qubits: int, reps: int = 0) -> QuantumCircuit:
-    assert num_of_qubits > 0
-    assert reps > 0
-    qregs = QuantumRegister(num_of_qubits)
-    cregs_array = [ClassicalRegister(num_of_qubits)]
-    for _ in range(reps):
-        cregs_array.append(ClassicalRegister(num_of_qubits))
-    qc = QuantumCircuit(qregs, *cregs_array)
-    single_snooping_window_qc = single_snooping_window(num_of_qubits)
-    conditional_reset_qc = conditional_reset(num_of_qubits)
-    for i in range(reps):
-        qc = qc.compose(single_snooping_window_qc, qregs, cregs_array[0])
-        if i < reps - 1:
-            qc.append(conditional_reset_qc, qregs, cregs_array[i])
-            qc.barrier()
-    return qc
+# def measure_all_and_conditional_reset(num_of_qubits: int):
+#     qregs = QuantumRegister(num_of_qubits)
+#     cregs = ClassicalRegister(num_of_qubits)
+#     qc = QuantumCircuit(qregs, cregs, name='meas all and reset')
+#     qc.measure(qregs, cregs)
+#     for i, creg in enumerate(cregs):
+#         with qc.if_test((creg, 1)):
+#             qc.x(qregs[i])
+#     return qc
+
+# def single_snooping_window(num_of_qubits: int) -> QuantumCircuit:
+#     assert num_of_qubits > 0
+#     qregs = QuantumRegister(num_of_qubits)
+#     cregs = ClassicalRegister(num_of_qubits)
+#     delay_dt = Parameter('snooping duration')
+#     qc = QuantumCircuit(qregs, cregs)
+#     qc.h(qregs)
+#     qc.delay(delay_dt, qregs)
+#     qc.h(qregs)
+#     qc.measure(qregs, cregs)
+#     return qc
+
+# def conditional_reset(num_of_qubits: int):
+#     qregs = QuantumRegister(num_of_qubits)
+#     cregs = ClassicalRegister(num_of_qubits)
+#     qc = QuantumCircuit(qregs, cregs, name="Conditional Reset")
+#     for i in range(num_of_qubits):
+#         with qc.if_test((cregs[i], 1)):
+#             qc.x(qregs[i])
+#     return qc
+
+# def create_snooping_circuit(num_of_qubits: int, reps: int = 0) -> QuantumCircuit:
+#     assert num_of_qubits > 0
+#     assert reps > 0
+#     qregs = QuantumRegister(num_of_qubits)
+#     cregs_array = [ClassicalRegister(num_of_qubits)]
+#     for _ in range(reps):
+#         cregs_array.append(ClassicalRegister(num_of_qubits))
+#     qc = QuantumCircuit(qregs, *cregs_array)
+#     single_snooping_window_qc = single_snooping_window(num_of_qubits)
+#     conditional_reset_qc = conditional_reset(num_of_qubits)
+#     for i in range(reps):
+#         qc = qc.compose(single_snooping_window_qc, qregs, cregs_array[0])
+#         if i < reps - 1:
+#             qc.append(conditional_reset_qc, qregs, cregs_array[i])
+#             qc.barrier()
+#     return qc
